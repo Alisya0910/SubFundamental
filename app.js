666a9f82a265472ba65f4a6b1b4824467ff7492d
Alisya0910 <115614262+Alisya0910@users.noreply.github.com>
2023-04-29 12:04:32 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime';
+import './styles/heroes-style.css';
+import "./script/component/app-bar.js";
+import './script/component/heroes-list.js';
+import main from './script/view/main.js';
+
+document.addEventListener('DOMContentLoaded', main);
